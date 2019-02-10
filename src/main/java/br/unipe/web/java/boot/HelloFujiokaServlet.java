0fa101a60306769737b15093e5a43efe42621a30
Rodrigo Fujioka <rodrigofujioka@gmail.com>
2019-02-10 17:40:14 -0200

    Inclusão de propriedades para não ficar dando erro após update do projeto. Inclusão de settinhs.xml para proxy de laboratórios, se for o caso. Inclusão de descritivo de identificação no pom.xml.

--- src/main/java/br/unipe/web/java/boot/HelloFujiokaServlet.java
+++ src/main/java/br/unipe/web/java/boot/HelloFujiokaServlet.java
@@ -9,26 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(
-        name = "FujiokaServlet",
-        urlPatterns = {"/fujioka"}
-    )
+@SuppressWarnings("serial")
+@WebServlet(name = "FujiokaServlet", urlPatterns = { "/fujioka" })
 public class HelloFujiokaServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, 
-    		HttpServletResponse resp)
-            throws ServletException, IOException {
-    	
-    	String parametro = req.getParameter("nome");
-    	if(parametro == null)
-    		 parametro = req.getAttribute("nome").toString();
-    	String idade = req.getParameter("idade");
-        ServletOutputStream out = resp.getOutputStream();
-        String saida = "Hello Fujioka " + parametro + " " + idade;
-        out.write(saida.getBytes());
-        out.flush();
-        out.close();
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String parametro = req.getParameter("nome");
+		if (parametro == null)
+			parametro = req.getAttribute("nome").toString();
+		String idade = req.getParameter("idade");
+		ServletOutputStream out = resp.getOutputStream();
+		String saida = "Hello Fujioka " + parametro + " " + idade;
+		out.write(saida.getBytes());
+		out.flush();
+		out.close();
+	}
 
 }
