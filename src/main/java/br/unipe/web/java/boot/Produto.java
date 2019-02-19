a707f217b234676e66854aff910ddf2d9c9275ba
Junior Mendes <junior.mendes369@gmail.com>
2019-02-19 00:25:14 -0300

    Subindo Modificações

---
+++ src/main/java/br/unipe/web/java/boot/Produto.java
@@ -0,0 +1,30 @@
+package br.unipe.web.java.boot;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "ProdutoServlet", urlPatterns = { "/produto" })
+public class Produto extends HttpServlet {
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String parametro = req.getParameter("nome");
+		if (parametro == null)
+			parametro = req.getAttribute("nome").toString();
+		String idade = req.getParameter("idade");
+		ServletOutputStream out = resp.getOutputStream();
+		String saida = "Hello Fujioka " + parametro + " " + idade;
+		out.write(saida.getBytes());
+		out.flush();
+		out.close();
+	}
+
+}
